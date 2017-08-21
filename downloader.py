a43e6ef0af492203cf4f069e389f87a09fe8a456
Aryeh Zapinsky <aryeh.zapinsky@columbia.edu>
2017-08-21 01:10:56 -0400

    Successfully generates list of song options from Zmirot Database (hardcoded)

--- downloader.py
+++ downloader.py
@@ -1,29 +1,62 @@
-import tkinter as tk
+import tkinter as tk, tkinter.scrolledtext
 import urllib.request
+from bs4 import BeautifulSoup
 
-
-def generate_song_list(path):
+def song_generator(path):
+    """
+    This function yields song_name and url address for each song in the list.
+    Currently the first result is a link
+    """
     with urllib.request.urlopen(path) as response:
-        return(response)
+        soup = BeautifulSoup(response.read(), 'html.parser')
+        content = soup.find("div", {"id":"content"})
+        yield from ((x.getText(),x.find('a').get('href'))
+                    for x in content.findAll('li'))
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
+        self.songs_states = {}
         self.pack()
         self.create_widgets()
+        self.create_scrolltext()
+
+    def create_scrolltext(self):
+        # Scrollable chechbuttons:
+        # https://stackoverflow.com/questions/5860675/variable-size-list-of-checkboxes-in-tkinter
+        self.song_frame = tk.Frame(self)
+        vsb = tk.Scrollbar(self.song_frame, orient="vertical")
+        text = tk.Text(self.song_frame, width=80, height=90,
+                            yscrollcommand=vsb.set)
+        vsb.config(command=text.yview)
+        vsb.pack(side="right", fill="y")
+        text.pack(side="left", fill="both", expand=True)
+        self.song_frame.pack()
+
+        for item in song_generator("http://www.zemirotdatabase.org/song_index.php"):
+            song, path = item
+            indicator = tk.IntVar()
+            self.songs_states.setdefault(song, (indicator, path))
+            cb = tk.Checkbutton(self.song_frame, variable=indicator, text=song)
+            text.window_create("end", window=cb)
+            text.insert("end", "\n")
+        text.config(state="disabled")
 
     def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+        self.update_songs = tk.Button(self)
+        self.update_songs["text"] = "Download songs"
+        self.update_songs["command"] = self.save_selected
+        self.update_songs.pack(side="top")
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=root.destroy)
         self.quit.pack(side="bottom")
 
-    def say_hi(self):
-        print("hi there, everyone!")
+    def save_selected(self):
+        for (key, value) in self.songs_states.items():
+            if value[0].get() == 1:
+                print(key)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
